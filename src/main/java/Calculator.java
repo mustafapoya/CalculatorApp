947c7a7a600cf650235b336f87ec9c2ac6be38b4
Mustafa Poya <mustafapoyaabc@gmail.com>
2022-11-03 16:01:38 +0430

    add division button

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -122,6 +122,27 @@ public class Calculator {
             }
         });
 
+        btnDiv = initBtn("/", x[3], y[1], event -> {
+            repaintFont();
+            if (Pattern.matches("([-]?\\d+[.]\\d*)|(\\d+)", inText.getText())) {
+                if(go) {
+                    val = calc(val, inText.getText(), opt);
+                    if (Pattern.matches("[-]?[\\d]+[.][0]*", String.valueOf(val))) {
+                        inText.setText(String.valueOf((int) val));
+                    } else {
+                        inText.setText(String.valueOf(val));
+                    }
+                    opt = '/';
+                    go = false;
+                    addWrite = false;
+                } else {
+                    opt = '/';
+                }
+            }
+        });
+
+        
+
         window.setLayout(null);
         window.setResizable(false);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Close button clicked? = End The process
