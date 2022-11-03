6cab0f2b98103f1d80c4d173e9891ddb123de5a8
Mustafa Poya <mustafapoyaabc@gmail.com>
2022-11-03 14:49:13 +0430

    define required variable and properties

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,61 @@
+import javax.swing.*;
+
+public class Calculator {
+    public static final int WINDOW_WIDTH = 410;
+    public static final int WINDOW_HEIGHT = 600;
+    public static final int BUTTON_WIDTH = 80;
+    public static final int BUTTON_HEIGHT = 70;
+    public static final int MARGIN_X = 20;
+    public static final int MARGIN_Y = 60;
+
+    private JFrame window;
+    private JComboBox<String> comboCalcType, comboTheme;
+    private JTextField inText;
+    private JButton btnC, btnBack, btnMod, btnDiv, btnMul, btnSub, btnAdd,
+            btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9,
+            btnPoint, btnEqual, btnRoot, btnPower, btnLog;
+
+    private char opt = ' '; // Save the operator
+    private boolean go = true; // For calculate with Opt != (=)
+    private boolean addWrite = true; // Connect numbers in display
+    private double val = 0; // Save the value typed for calculation
+
+    /*
+        Mx Calculator:
+        X = Row
+        Y = Column
+
+        +-------------------+
+        |   +-----------+   |   y[0]
+        |   |           |   |
+        |   +-----------+   |
+        |                   |
+        |   C  <-   %   /   |   y[1]
+        |   7   8   9   *   |   y[2]
+        |   4   5   6   -   |   y[3]
+        |   1   2   3   +   |   y[4]
+        |   .   0     =     |   y[5]
+        +-------------------+
+         x[0] x[1] x[2] x[3]
+    */
+
+    /*
+        +-------------------+
+        |   +-----------+   |   y[0]
+        |   |           |   |
+        |   +-----------+   |
+        |                   |
+        |   0   1   1   3   |   y[1]
+        |   4   5   6   7   |   y[2]
+        |   8   9   10  11  |   y[3]
+        |   12  13  14  15  |   y[4]
+        |   16  17    18    |   y[5]
+        +-------------------+
+         x[0] x[1] x[2] x[3]
+
+    */
+
+    public Calculator() {
+        
+    }
+}
