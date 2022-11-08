ee5533fb29b218118804da8309b19923837f5b8e
Mustafa Poya <mustafapoyaabc@gmail.com>
2022-11-08 14:33:40 +0430

    add operation

--- src/main/java/CalculatorViewController.java
+++ src/main/java/CalculatorViewController.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -26,15 +27,41 @@ public class CalculatorViewController implements Initializable {
     btnLog, btnDivision, btnMultiply, btnSubtract, btnAdd, btnEqual, btnPoint, btnSign,
     btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
 
+    private char operator = ' '; // Save the operator
+    private boolean go = true; // For calculate with Opt != (=)
+    private boolean addWrite = true; // Connect numbers in display
+    private double val = 0; // Save the value typed for calculation
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        lblText.setText("0");
 
-        toggleTheme.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                System.out.println("Changing Theme");
+        btnC.setOnAction(event-> {
+            System.out.println("BTN C");
+
+            lblText.setText("0");
+            operator = ' ';
+            val = 0;
+        });
+
+        btnBack.setOnAction(event -> {
+            String str = lblText.getText();
+
+            StringBuilder str2 = new StringBuilder();
+            for(int i = 0; i < str.length() - 1; i++) {
+                str2.append(str.charAt(i));
+            }
+
+            if(str2.toString().equals("")) {
+                lblText.setText("0");
+            } else {
+                lblText.setText(str2.toString());
             }
         });
+
+        btnRemainder.setOnAction();
+
     }
 
 }
